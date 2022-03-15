386b306b295622c0d0ddc7bf1aefcef1723bb8a4
Santosh0208 <santoshalamuru0@msitprogram.net>
2022-03-15 09:16:06 +0530

    peek

--- Queue.java
+++ Queue.java
@@ -46,6 +46,13 @@ class Queue {
     
     // Get the front element.
     public int peek() {
+        if (!s2.isEmpty()) {
+            return s2.peek();
+        } else {
+            while (!s1.isEmpty())
+                s2.push(s1.pop());
+        }
+        return s2.peek();
 
     }
     
